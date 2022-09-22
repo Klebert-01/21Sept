fd35cb0742217cde0e47bd9c572ece86dadc38fb
Klebert-01 <arvid.berkert@gmail.com>
2022-09-22 09:43:27 +0200

    backup innan ändring

--- 21Sept/Program.cs
+++ 21Sept/Program.cs
@@ -1,2 +1,6 @@
 ﻿// Onsdag 21 September
+using _21Sept;
+
+
+IGuest[] guestList = new IGuest[] {new Collegue("Joel"), new Friend("Janne"), new Neighbor("Jens"), new Friend("Jessica"), new Collegue("Johanna")};
 
